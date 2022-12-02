736d867c0f2d6fd30c16354a3ed70a2d060d6340
Ksesha <bouncywolf@list.ru>
2022-12-02 16:08:48 +0300

    done task6

---
+++ Task6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Это число чётное? Введите число: " );
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
