55a29b019b1d4e0b1386dbb71620e5cdb26c6bb4
Ksesha <bouncywolf@list.ru>
2022-12-02 17:31:23 +0300

    done task8

---
+++ Task8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+for (int i = 2; i <= n; i++) {
+    if (i % 2 == 0) {
+        Console.WriteLine("{0}", i);
+    }
+}
