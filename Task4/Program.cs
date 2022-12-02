56728896c70f2dda2d5f694b49c7aa76a9baf8fd
Ksesha <bouncywolf@list.ru>
2022-12-02 15:46:55 +0300

    done task4

---
+++ Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine($"max = " + a);
+}
+else if (b > c)
+{
+    Console.WriteLine($"max = " + b);
+}
+else if (c > b)
+{
+    Console.WriteLine($"max = " + c);
+}
