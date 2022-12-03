5f03f980eba466abe037df4626284868741766a6
unknown <bouncywolf@list.ru>
2022-12-03 19:31:43 +0300

    almost done task13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n % 10;
+
+if (n <= 99)
+Console.WriteLine("Третьего числа нет");
+
+    {
+        if (n >= 100 && n <= 999)
+        Console.WriteLine(m);
+    }
+while (n > 1000)
+{
+    n = n / 10;
+    Console.WriteLine(m);
+}
+
+// осталась проблема с более чем 3-х значными числами, не могу найти проблему в цикле вайл
