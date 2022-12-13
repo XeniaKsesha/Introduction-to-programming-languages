f1089c5e2070961e98c8855dd0edd13859009afb
Ksesha <bouncywolf@list.ru>
2022-12-13 17:39:47 +0300

    done task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 1;
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= m; i++)
+result = result * n;
+Console.WriteLine(result);
