062f764c6333c29befe16de114a3d1256d34bf3c
Ksesha <bouncywolf@list.ru>
2022-12-13 18:44:51 +0300

    done task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите элемент для массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine($"[{String.Join("; ", array)}]");
