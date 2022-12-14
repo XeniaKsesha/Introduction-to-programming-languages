eec75b7093f04a496af10dbdccdafa3faa42f211
Ksesha <bouncywolf@list.ru>
2022-12-14 17:06:25 +0300

    done task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,26 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 != 0)
+        sum = sum + array[i];
+    }
+    Console.WriteLine($"Сумма нечетных чисел в массиве равна: {sum}");
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array= new int [n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
