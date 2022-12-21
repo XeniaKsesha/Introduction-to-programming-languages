cd770557bc1c4b54d74ef736591b80bb9f3ed3a9
Ksesha <bouncywolf@list.ru>
2022-12-21 13:52:01 +0300

    done task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+int [] s = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int result = 0;
+for (int i = 0; i < s.Length; i++)
+{
+    if (s [i] > 0)
+    result = result + 1;
+}
+Console.WriteLine($"Чисел больше нуля: {result}");
