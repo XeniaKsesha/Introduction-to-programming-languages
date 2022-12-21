9f4c0e6cfe2eaff087c7d1752319ac0f4b2ccd5e
Ksesha <bouncywolf@list.ru>
2022-12-21 19:50:41 +0300

    done task 50

---
+++ Task50/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите индекс строки массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите индекс столбца массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+}
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "\t  ");
+        Console.WriteLine();
+}
+
+Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>m && b>n)
+ Console.WriteLine("Такого числа нет");
+ else
+ Console.WriteLine(array [a - 1, b - 1]);
