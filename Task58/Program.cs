053a804a101483afd5b14efcfd832bbb1604189d
Ksesha <bouncywolf@list.ru>
2022-12-26 17:31:36 +0300

    done or almost done task 58

---
+++ Task58/Program.cs
@@ -0,0 +1,67 @@
+﻿//хз правильно ли работает
+
+void InputMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix [i, j] = new Random().Next(1, 11);
+    }
+}
+
+
+void PrintMatrix(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum = sum + firstMartrix[i,k] * secondMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите размеры матриц и диапазон случайных значений:");
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы и строк 2-й матрицы: ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int[,] firstMartrix = new int[m, n];
+InputMatrix(firstMartrix);
+Console.WriteLine("Первая матрица:");
+PrintMatrix(firstMartrix);
+
+int[,] secondMartrix = new int[n, p];
+InputMatrix(secondMartrix);
+Console.WriteLine("Вторая матрица:");
+PrintMatrix(secondMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+MultiplyMatrix(firstMartrix, secondMartrix, resultMatrix);
+Console.WriteLine("Произведение первой и второй матриц:");
+PrintMatrix(resultMatrix);
