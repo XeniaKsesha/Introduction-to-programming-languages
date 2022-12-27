1f8217b914d34eb15c554a250715ebca2ef3b1a7
Ksesha <bouncywolf@list.ru>
2022-12-27 16:34:29 +0300

    done task 68

---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿int akkerman(int n, int m)
+{
+ if (n == 0)
+ return m + 1;
+
+ else if (m == 0 && n > 0)
+ return akkerman(n - 1, 1);
+
+ else
+ return (akkerman(n - 1, akkerman(n, m - 1)));
+}
+
+Console.Clear();
+Console.Write("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(akkerman(n, m));
